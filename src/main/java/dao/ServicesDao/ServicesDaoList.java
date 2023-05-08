f5685b5bce452df2666345eff85f46c2f2291728
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-05-08 20:53:39 -0300

    Update ServicesDaoList.java
    Creating servicesDao constructor

--- src/main/java/dao/ServicesDao/ServicesDaoList.java
+++ src/main/java/dao/ServicesDao/ServicesDaoList.java
@@ -1,5 +1,14 @@
 package dao.ServicesDao;
 
+import javafx.concurrent.Service;
+
+import java.util.ArrayList;
+
 public class ServicesDaoList  {
+    private ArrayList<Service> listServices = new ArrayList<Service>();
+    public ServicesDaoList(ArrayList<Service> listServices){
+        this.listServices = listServices;
+
+    }
 
 }
