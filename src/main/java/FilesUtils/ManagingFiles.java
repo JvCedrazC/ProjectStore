a3f3fbf1370cb5ab3e924506f09e2bd569f977f9
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-05-08 12:04:07 -0300

    Update ManagingFiles.java
    Creating save function

--- src/main/java/FilesUtils/ManagingFiles.java
+++ src/main/java/FilesUtils/ManagingFiles.java
@@ -1,8 +1,30 @@
 package FilesUtils;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
 public class ManagingFiles {
 
      private String filename;
 
-     
+     public ManagingFiles(String name){
+         this.filename = name;
+     }
+
+     public <T> void save(ArrayList<T> list){
+         try {
+             FileOutputStream file = new FileOutputStream(this.filename);
+             ObjectOutputStream object = new ObjectOutputStream(file);
+             object.writeObject(list);
+             object.close();
+
+         }catch (IOException e){
+             throw new RuntimeException(e);
+         }
+     }
+     public <T> ArrayList<T> retrieve(ArrayList<T> list){
+         
+     }
 }
