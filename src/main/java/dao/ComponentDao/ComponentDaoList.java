d15419b698e287bab2424c361ad391a999f75001
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-05-09 02:09:49 -0300

    Update ComponentDaoList.java
    Change list implementation to file implementation

--- src/main/java/dao/ComponentDao/ComponentDaoList.java
+++ src/main/java/dao/ComponentDao/ComponentDaoList.java
@@ -1,26 +1,37 @@
 package dao.ComponentDao;
 
 
+import FilesUtils.ManagingFiles;
 import Model.Component;
+import Model.Customer;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class ComponentDaoList implements ComponentDAO{
-    private ArrayList<Component> lista = new ArrayList<Component>();
+    private ManagingFiles managingFiles;
     private int nextID;
     public ComponentDaoList(){
-        this.lista = new ArrayList<Component>();
-        this.nextID = 0;
+        this.managingFiles = new ManagingFiles("Component.bin");
+        ArrayList<Component> lista = this.managingFiles.retrieve();
+        if (lista.size() == 0){
+            this.nextID = lista.size();
+        } else{
+            this.nextID = 0;
+        }
     }
     @Override
     public Component create(Component component){
+        ArrayList<Component> lista = this.managingFiles.retrieve();
         component.setId(nextID);
         nextID++;
         lista.add(component);
+        managingFiles.save(lista);
         return component;
     }
     @Override
     public Component findById(int id){
+        ArrayList<Component> lista = this.managingFiles.retrieve();
         for (Component i: lista) {
             if (i.getId() == id){
                 return i;
@@ -30,8 +41,9 @@ public class ComponentDaoList implements ComponentDAO{
     }
     @Override
     public List<Component> findMany(){
+        ArrayList<Component> lista = this.managingFiles.retrieve();
         ArrayList<Component> listComponent = new ArrayList<Component>();
-        for (Object o : this.lista) {
+        for (Object o : lista) {
             listComponent.add((Component) o);
         }
         return listComponent;
@@ -39,24 +51,29 @@ public class ComponentDaoList implements ComponentDAO{
 
     @Override
     public void deleteById(int id){
+        ArrayList<Component> lista = this.managingFiles.retrieve();
         for (int i = 0; i < lista.size(); i++) {
-            if (this.lista.get(i).getId() == id) {
-                this.lista.remove(i);
+            if (lista.get(i).getId() == id) {
+                lista.remove(i);
                 break;
             }
         }
+        managingFiles.save(lista);
     }
     public void update(Component component){
-        for (int i = 0; i < this.lista.size(); i++) {
-            if (this.lista.get(i).getId() == component.getId()) {
-                this.lista.set(i, component);
+        ArrayList<Component> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == component.getId()) {
+                lista.set(i, component);
                 return;
             }
         }
+        managingFiles.save(lista);
     }
     public void deleteMany(){
-        this.lista = new ArrayList<Component>();
+        ArrayList<Component> lista  = new ArrayList<Component>();
         this.nextID =  0;
+        managingFiles.save(lista);
     }
 
 }
