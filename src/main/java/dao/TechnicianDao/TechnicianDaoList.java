84ca145ccc9748dc11ddbe0ac098d6a3aae1a4eb
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-05-09 02:10:00 -0300

    Update TechnicianDaoList.java

--- src/main/java/dao/TechnicianDao/TechnicianDaoList.java
+++ src/main/java/dao/TechnicianDao/TechnicianDaoList.java
@@ -1,32 +1,41 @@
 package dao.TechnicianDao;
 
 
+import FilesUtils.ManagingFiles;
 import Model.Technician;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class TechnicianDaoList implements TechnicianDAO{
-    private ArrayList<Technician> lista = new ArrayList<Technician>();
+    private ManagingFiles managingFiles;
     private int nextID;
 
     public TechnicianDaoList(){
-        this.lista = new ArrayList<Technician>();
-        this.nextID = 0;
+        this.managingFiles = new ManagingFiles("Technician.bin");
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        if (lista.size() == 0){
+            this.nextID = lista.size();
+        } else{
+            this.nextID = 0;
+        }
     }
 
     @Override
     public Technician create(Technician technician){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
         technician.setId(nextID);
         nextID++;
-        this.lista.add(technician);
+        lista.add(technician);
+        save(lista);
         return technician;
 
     }
 
     @Override
     public Technician findById(int id) {
-        for (Technician technician: this.lista) {
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (Technician technician: lista) {
             if (technician.getId() == id){
                 return technician;
             }
@@ -35,8 +44,9 @@ public class TechnicianDaoList implements TechnicianDAO{
     }
     @Override
     public ArrayList<Technician> findMany(){
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
         ArrayList<Technician> listTechnician = new ArrayList<Technician>();
-        for (Object o : this.lista) {
+        for (Object o : lista) {
             listTechnician.add((Technician) o);
         }
         return listTechnician;
@@ -44,38 +54,50 @@ public class TechnicianDaoList implements TechnicianDAO{
 
     @Override
     public void update(Technician technician){
-        for (int i = 0; i < this.lista.size(); i++) {
-            if (this.lista.get(i).getId() == technician.getId()) {
-                this.lista.set(i, technician);
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == technician.getId()) {
+                lista.set(i, technician);
+                save(lista);
                 return;
             }
         }
     }
     @Override
     public void deleteById(int id){
-        for (int i = 0; i < this.lista.size(); i++) {
-            if (this.lista.get(i).getId() == id) {
-                this.lista.remove(i);
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getId() == id) {
+                lista.remove(i);
                 break;
             }
         }
         changeId();
+        save(lista);
     }
     public void deleteMany(){
-        this.lista = new ArrayList<Technician>();
+        ArrayList<Technician> lista = new ArrayList<Technician>();
         this.nextID =  0;
+        save(lista);
     }
     public Technician findByName(String name){
-        for (Technician i: this.lista) {
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (Technician i: lista) {
             if (i.getName() == name){
                 return i;
             }
         }return null;
     }
     private void changeId(){
-        for (int i = 0; i < this.lista.size();i++){
-            this.lista.get(i).setId(i);
+        ArrayList<Technician> lista = this.managingFiles.retrieve();
+        for (int i = 0; i < lista.size();i++){
+            lista.get(i).setId(i);
         }
+        save(lista);
+    }
+    public void save(ArrayList<Technician> lista){
+
+        managingFiles.save(lista);
     }
 
 }
