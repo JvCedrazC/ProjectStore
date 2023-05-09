fb47367af322fe7322703a3a859cf52fab9a26d7
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-05-09 02:06:21 -0300

    Update TestComponentDao.java
    testing create method

--- src/main/test/java/DaoTest/TestComponentDao.java
+++ src/main/test/java/DaoTest/TestComponentDao.java
@@ -1,4 +1,22 @@
 package DaoTest;
 
+import Model.Component;
+import dao.ComponentDao.ComponentDaoList;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestComponentDao {
+    @Test
+    public void create(){
+        ComponentDaoList componentList = new ComponentDaoList();
+        componentList.deleteMany();
+        Component component1 = new Component(0, 20.0, 30.0, "RAM");
+        componentList.create(component1);
+        assertEquals(0, componentList.findById(0).getId());
+
+
+    }
 }
