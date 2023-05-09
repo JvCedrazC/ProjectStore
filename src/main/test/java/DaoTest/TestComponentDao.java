a23794be699bcb22b55e542b407c247f854c4f93
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2023-05-09 02:23:50 -0300

    Update TestComponentDao.java
    Update update method

--- src/main/test/java/DaoTest/TestComponentDao.java
+++ src/main/test/java/DaoTest/TestComponentDao.java
@@ -16,7 +16,30 @@ public class TestComponentDao {
         Component component1 = new Component(0, 20.0, 30.0, "RAM");
         componentList.create(component1);
         assertEquals(0, componentList.findById(0).getId());
-
-
     }
+
+    @Test
+    public void findById(){
+        ComponentDaoList componentList = new ComponentDaoList();
+        componentList.deleteMany();
+        Component component1 = new Component(0, 20.0, 30.0, "RAM");
+        Component component2 = new Component(1, 25.0, 40.0, "PROCESSADOR");
+        componentList.create(component1);
+        componentList.create(component2);
+        assertEquals(20.0, componentList.findById(0).getCost());
+        assertEquals(25.0, componentList.findById(1).getCost());
+    }
+    @Test
+    public void findMany(){
+        ComponentDaoList componentList = new ComponentDaoList();
+        assertEquals(2, componentList.findMany().size());
+    }
+    @Test
+    public void update(){
+        ComponentDaoList componentList = new ComponentDaoList();
+        Component component1 = new Component(0, 25.0, 38.0, "RAM");
+        componentList.update(component1);
+        assertEquals(38.0, componentList.findById(0).getPrice());
+    }
+
 }
